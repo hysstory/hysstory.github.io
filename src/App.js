0242df66ec1b36d2758c1e40c3df1a176480e71f
hysstory <dyhan92@gmail.com>
2021-02-19 17:52:52 -0500

    temporary

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Construction from './components/Construction'
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Construction />
+    </div>
+  )
+}
+
+export default App;
