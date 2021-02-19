dd00e5421791b8c0c04169d7ca14d4ebd96b0da0
hysstory <dyhan92@gmail.com>
2021-02-19 18:13:23 -0500

    temporary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import AnimatedCursor from 'react-animated-cursor'
+
 import Construction from './components/Construction'
 
 import './App.css';
@@ -7,6 +9,8 @@ import './App.css';
 function App() {
   return (
     <div>
+      <AnimatedCursor
+        color='176,224,230' />
       <Construction />
     </div>
   )
